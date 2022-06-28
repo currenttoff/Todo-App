9f5f636872a0fda3a7e20ad6184549427bdbc9c1
Mohammed Thoufiq <101857119+currenttoff@users.noreply.github.com>
2022-06-28 19:59:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import "./App.css";
-// import ComA from "./hooks/useContext/ComA";
-// import CleanUp from "./hooks/useEffectHook/CleanUp";
-// import GithubUsers from "./hooks/useEffectHook/GithubUsers";
-// import GithubUsers2 from "./hooks/useEffectHook/GithubUsers2";
-// import CounterReducer from "./hooks/useReducer/CounterReducer";
-// import Todo from "./TodoApp/todo";
-// import Todo2 from "./TodoApp/todo2";
+
 import Todo3 from "./TodoApp/todo3";
-// import Uncontrolled from "./hooks/useRefHook/Uncontrolled";
-// import UseEffects1 from "./hooks/useEffectHook/UseEffects1";
-// import UseEffects2 from "./hooks/useEffectHook/UseEffects2";
-// import UseEffects3 from "./hooks/useEffectHook/UseEffects3";
+
 
 function App() {
   return (
